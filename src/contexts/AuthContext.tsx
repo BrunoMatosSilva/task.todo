e11110c2aafb0cb37627aed4de199b79e23304ea
BrunoMatosSilva <brunomatos2010@outlook.com>
2021-08-20 17:35:34 -0300

    user context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,14 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 import { supabase } from '../services/supabaseClient'
 
+type User = {
+    id: string;
+    name: string;
+    avatar: string;
+}
+
 type AuthContextType = {
+    user: User | undefined,
     signInWithGoogle: () => Promise<void>;
     signOut: () => Promise<void>;
 }
@@ -14,11 +21,13 @@ export const AuthContext = createContext({} as AuthContextType)
 
 export function AuthContextProvider(props: AuthContextProviderProps) {
 
+    const [user, setUser] = useState<User>();
+
+
     async function signInWithGoogle() {
-        const { user, session, error } = await supabase.auth.signIn({
+
+        const usuario = await supabase.auth.signIn({
             provider: 'google',
-        }, {
-            redirectTo: 'http://localhost:3000/task'
         });
     }
 
@@ -27,7 +36,7 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
     }
 
     return (
-        <AuthContext.Provider value={{ signInWithGoogle, signOut }}>
+        <AuthContext.Provider value={{ signInWithGoogle, signOut, user }}>
             {props.children}
         </AuthContext.Provider>
     );
--- src/pages/task/index.tsx
+++ src/pages/task/index.tsx
@@ -4,8 +4,6 @@ import { Header } from "../../components/Header";
 import { User } from "../../components/User";
 import { ContentNewTask, ContentTask } from "../../styles/Task";
 
-import Modal from 'react-modal';
-
 import { AiOutlineSearch, AiOutlineFilter } from "react-icons/Ai";
 import { BiTask } from "react-icons/Bi";
 import { Cards } from "../../components/Cards";
