333bd94c5073e86ba2c1bc9da4f202f3f04f436c
BrunoMatosSilva <brunomatos2010@outlook.com>
2021-08-25 17:49:29 -0300

    signOutgoogle

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,11 @@ import { GoSignOut } from 'react-icons/Go';
 import Link from 'next/link';
 
 import { HeaderContent } from './styles';
+import { auth } from '../../services/firebase';
 
+async function signOutWithGoogle() {
+    await auth.signOut();
+}
 
 export function Header() {
 
@@ -12,7 +16,7 @@ export function Header() {
                 <div>
                     <span><img src="/logo_small.svg" /></span>
                     <Link href="/">
-                        <button><GoSignOut /></button>
+                        <button onClick={signOutWithGoogle}><GoSignOut /></button>
                     </Link>
                 </div>
             </HeaderContent>
